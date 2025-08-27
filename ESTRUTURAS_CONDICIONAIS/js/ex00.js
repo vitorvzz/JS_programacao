ae66bbae5f3a36fda660f7d39466d987f7bb4810
vitorvzz <vzdogueto@gmail.com>
2025-08-27 15:05:31 -0300

    ex00
    verificar idade CNH

---
+++ ESTRUTURAS_CONDICIONAIS/js/ex00.js
@@ -0,0 +1,12 @@
+var nome, idade
+
+nome = prompt("digite seu nome:")
+idade = parseInt (prompt("digite sua idade"))
+
+if(idade == 18){
+alert(nome+"voce esta apto a tirar a CNH")
+}
+
+else{
+alert(nome+" voce ainda nao tem idade pra tirar a CNH")
+}
